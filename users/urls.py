91e34d799d3ba58e6be47323cf11ad2b001d974e
prosper <edwardprosper001@gmail.com>
2022-08-12 14:52:34 -0700

    updates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import signup
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("signup", signup, name="signup")
+    path("signup/", signup, name="signup"),
+    path("signin/", auth_views.LoginView.as_view(template_name="users/login.html"), name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 def signup(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hi {username}, your account has been created successfully!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'users/signup.html', context)
+
+
