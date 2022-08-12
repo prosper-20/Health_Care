d221250a0ae61338e3e994e50917be69cfd4448f
prosper <edwardprosper001@gmail.com>
2022-08-12 13:46:10 -0700

    updates

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["HEALTH_CARE_SECRET_KEY"]
+SECRET_KEY = os.environ.get("HEALTH_CARE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
