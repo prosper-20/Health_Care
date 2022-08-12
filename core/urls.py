7e5e60870d5125e23a08382301c06f03b8cbc19d
prosper <edwardprosper001@gmail.com>
2022-08-12 14:07:16 -0700

    updates

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home
 
-urlapatterns = [
+urlpatterns = [
     path("", home, name="home")
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import  signup
+from .views import signup
 
-urlapatterns = [
+urlpatterns = [
     path("signup", signup, name="signup")
 ]
