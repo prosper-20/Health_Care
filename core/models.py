f4502691bc9147ea3fe02c32c1632e1050f5612c
prosper <edwardprosper001@gmail.com>
2022-08-14 17:45:54 -0700

    fixes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+SERVICES_CHOICES = (
+    (
+        ("Gym Session", "Gym Session"),
+        ("Therapy Session", "Therapy Session"),
+        ("Massage", "Massage"),
+        ("Spa", "Spa")
+    )
+)
+
+
+TIME_CHOICES = (
+    ("8:00AM", "8:00AM"),
+    ("9:00AM", "9:00 AM"),
+    ("10:00AM", "10:00 AM"),
+    ("11:00AM", "11:00 AM"),
+    ("12:00PM", "12:00 PM"),
+    ("1:00PM", "1:00 PM"),
+    ("2:00PM", "2:00 PM"),
+    ("3:00PM", "3:00 PM"),
+    ("4:00PM", "4:00 PM"),
+    ("5:00PM", "5:00 PM"),
+    ("6:00PM", "6:00 PM"),
+    ("7:00PM", '7:00 PM')
+)
+
+
+class Consultation(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    services = models.CharField(choices=SERVICES_CHOICES, max_length=100)
+    date = models.
+    time = models.CharField(choices=TIME_CHOICES, max_length=50)
+
