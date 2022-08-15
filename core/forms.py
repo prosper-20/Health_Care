6c38984cac8eb21a325bc59ba7aaa9e0e05c266e
prosper <edwardprosper001@gmail.com>
2022-08-14 18:01:35 -0700

    fixes

--- core/forms.py
+++ core/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import Consultation
+
+
+class ConsultationForm(forms.ModelForm):
+    class Meta:
+        model = Consultation
+
+        fields = ["first_name", "last_name", "service", "date", "time"]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .forms import ConsultationForm
+from .models import Consultation
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+def consultation(request):
+    if request.method == "POST":
+        form = ConsultationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, )
 
 
