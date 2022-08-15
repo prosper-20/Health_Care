96dc797d2145aa038dbc20473381e6bfce2eec2b
prosper <edwardprosper001@gmail.com>
2022-08-15 13:12:09 -0700

    updates

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, consultation, Home, subscription
+from .views import consultation, Home, subscription
 
 urlpatterns = [
-    path('home/', Home, name="home_1"),
-    path("", home, name="home"),
+    path("", Home, name="home"),
     path("consultation/", consultation, name="consultation"),
     path('subscribe/', subscription, name="subscription")
 ]
--- core/views.py
+++ core/views.py
@@ -7,27 +7,27 @@ from django.contrib import messages
 
 # Create your views here.
 
-def home(request):
-    if request.method == "POST":
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get('last_name')
-        service = request.POST.get("service")
-        date = request.POST.get("date")
-        time = request.POST.get("time")
+# def home(request):
+#     if request.method == "POST":
+#         first_name = request.POST.get("first_name")
+#         last_name = request.POST.get('last_name')
+#         service = request.POST.get("service")
+#         date = request.POST.get("date")
+#         time = request.POST.get("time")
 
-        consultation = Consultation.objects.create(
-            first_name = first_name,
-            last_name = last_name,
-            service = service,
-            date = date,
-            time = time
-        )
-        consultation.save()
-        messages.success(request, f"Hi {first_name}, you have successfully booked a session with us.. see you soon")
-        return redirect("home")
+#         consultation = Consultation.objects.create(
+#             first_name = first_name,
+#             last_name = last_name,
+#             service = service,
+#             date = date,
+#             time = time
+#         )
+#         consultation.save()
+#         messages.success(request, f"Hi {first_name}, you have successfully booked a session with us.. see you soon")
+#         return redirect("home")
     
-    else:
-        return render(request, 'core/home.html')
+#     else:
+#         return render(request, 'core/home.html')
 
 
 def Home(request):
