b03f659d02f74c386b14d6b9b21d17fcd38228be
prosper <edwardprosper001@gmail.com>
2022-08-15 13:37:49 -0700

    updates

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import consultation, Home, subscription
+from .views import (
+    consultation,
+    Home, 
+    subscription,
+    BMIChecker
+)
 
 urlpatterns = [
     path("", Home, name="home"),
     path("consultation/", consultation, name="consultation"),
-    path('subscribe/', subscription, name="subscription")
+    path('subscribe/', subscription, name="subscription"),
+    path("bmi/checker/", BMIChecker, name="bmi_checker")
 ]
