5f0f551ec9062867fe545d499562406a41303e92
prosper <edwardprosper001@gmail.com>
2022-08-15 04:32:53 -0700

    updates

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, consultation
+from .views import home, consultation, Home
 
 urlpatterns = [
+    path('home/', Home, name="home_1"),
     path("", home, name="home"),
     path("consultation/", consultation, name="consultation")
 ]
