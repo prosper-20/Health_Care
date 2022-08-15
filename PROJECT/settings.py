43d3d6f19c013e5379bd11456959250e018086f0
prosper <edwardprosper001@gmail.com>
2022-08-15 03:55:35 -0700

    modifications

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'PROJECT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'EddiePdgr8',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
