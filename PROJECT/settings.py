2abcddb3b67970a251d664e99d2a00d2db220c5e
prosper <edwardprosper001@gmail.com>
2022-08-17 11:31:50 -0700

    Blog

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
-    'crispy_forms'
+    'crispy_forms',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
