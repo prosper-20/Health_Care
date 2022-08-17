923d3a8a8197c23c55d47a89cbe60807affff602
prosper <edwardprosper001@gmail.com>
2022-08-17 11:48:05 -0700

    Blog

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,14 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Post()
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField()
+    
+
+    def __str__(self):
+        return self.title
