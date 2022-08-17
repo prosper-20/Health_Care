a605a749807f2de51a70e2d6eb9b93bc19029397
prosper <edwardprosper001@gmail.com>
2022-08-17 12:17:32 -0700

    Blog

--- blog/models.py
+++ blog/models.py
@@ -20,4 +20,4 @@ class Post(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse("deatil", args=[str(self.slug)])
+        return reverse("detail", args=[str(self.slug)])
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, detail
 
 urlpatterns = [
-    path("", home, name="blog-home")
+    path("", home, name="blog-home"),
     path("<slug:slug>/", detail, name="detail")
 ]
