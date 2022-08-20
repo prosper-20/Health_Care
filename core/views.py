fc574f4ddf09dd9bd05e7122f1b53af61278d313
prosper <edwardprosper001@gmail.com>
2022-08-20 13:50:44 -0700

    updates

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ConsultationForm, SubscriptionForm, BMIForm, RoutineForm, ContactForm
-from .models import Consultation, Personalization, Subscription, BMI, Question, Contact, Coach, Service, Testimonies, Post
+from .models import Consultation, Personalization, Subscription, BMI, Question, Contact, Coach, Service, Testimonies
+from blog.models import Post
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
