4e75c290b64abcff94232739239cf2d8cd681016
prosper <edwardprosper001@gmail.com>
2022-08-29 12:49:27 +0100

    new template

--- core/urls.py
+++ core/urls.py
@@ -24,14 +24,14 @@ urlpatterns = [
     path("consultation/", consultation, name="consultation"),
     path('subscribe/', subscription, name="subscription"),
     path("bmi/checker/", BMIChecker, name="bmi_checker"),
-    path("services", services, name="services"),
+    path("services", service, name="services"),
     path("my-routine/", routine, name="routine"),
     path("create/", RoutineCreateView.as_view(), name="create"),
     path("contact/", contact_main, name="contact"),
     path("about/", about, name="about"),
     path("coach/", coach, name="coach"),
     path("service", service, name="service"),
-    path("service/<slug:slug>/", service_detail, name="servcie-detail"),
+    path("service/<slug:slug>/", service_detail, name="service-detail"),
     path("pricing", pricing, name="pricing"),
     path("success-stories/", success_stories, name="success-stories")
    
